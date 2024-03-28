f7234813f602027b7593ea726b6a58e6b0b96eaa
abMayorga <dab4se@gmail.com>
2024-03-27 23:30:31 -0700

    Revert "Finalized-ish"
    This reverts commit 48cac60a6fc51843f648851c83d12087f55819e9.

--- app/src/main/java/com/example/a133gamesnake/GameObject.java
+++
@@ -1,9 +0,0 @@
-package com.example.a133gamesnake;
-
-import android.graphics.Canvas;
-import android.graphics.Paint;
-
-// Interface representing a game object
-interface GameObject {
-    void draw(Canvas canvas, Paint paint);
-}

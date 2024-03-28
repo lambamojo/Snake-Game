48cac60a6fc51843f648851c83d12087f55819e9
abMayorga <dab4se@gmail.com>
2024-03-27 23:14:54 -0700

    Finalized-ish

---
+++ app/src/main/java/com/example/a133gamesnake/GameObject.java
@@ -0,0 +1,9 @@
+package com.example.a133gamesnake;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+// Interface representing a game object
+interface GameObject {
+    void draw(Canvas canvas, Paint paint);
+}
